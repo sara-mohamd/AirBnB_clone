7640aaef9395af53576419d310b2988be096b0a0
sara-mohamd <saraamohamed0038@gmail.com>
2024-03-06 22:32:58 +0200

    model files

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""BaseModel"""
+import cmd
+
+
+class BaseModel(cmd.Cmd):
+    """
+    defines all common attributes/methods for other classes
+    """
+    def __init(self):
+        ...

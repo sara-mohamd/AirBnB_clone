d31bd41da34db2188a952e43627dab4e1ec4b78c
sara-mohamd <saraamohamed0038@gmail.com>
2024-03-07 18:41:59 +0200

    Test

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """BaseModel"""
 import cmd
+from uuid import uuid4
+import datetime as dt
 
 
 class BaseModel(cmd.Cmd):
@@ -8,4 +10,27 @@ class BaseModel(cmd.Cmd):
     defines all common attributes/methods for other classes
     """
     def __init(self):
-        ...
+        """ Base Constructor """
+        self.id = str(uuid4())
+        self.created_at = dt.datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """return object info"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = dt.datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values """
+        dictionary = {}
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                value = value.isoformat("%Y-%m-%dT%H:%M:%S.%f")
+            dictionary[key] = value
+        dictionary['__class__'] = self.__class__.__name__
+        return dictionary
